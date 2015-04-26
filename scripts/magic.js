b43e14175bcfd19eb61f0165c10bffd24f99fa56
fushigishi <fushigishi@gmail.com>
2015-04-26 02:06:51 -0300

    Tabela

--- scripts/magic.js
+++ scripts/magic.js
@@ -2,9 +2,22 @@
 
 
 $(document).ready(function(){
-	$('#gema-table').html('FANCY TABLE~')
+	var tableDiv = $('#gema-table')
+	var table = $('<table>').appendTo(tableDiv) 
+	console.log(table)
+	// var text = ''
 	$.get('table/gema_table', function(data) {
-		alert(data)
+		var lines = data.split('\n')
+		console.log(lines.length)
+		for (var i = 0; i < lines.length - 1; ++i) {
+			var tableLine = $('<tr>').appendTo(table)
+			console.log(lines[i])
+			var fields = lines[i].split(" ")
+			for (var j = 0; j < fields.length; ++j) {
+				var tableCell = $('<td>').appendTo(tableLine)
+				tableCell.html(fields[j])
+			}
+		}
 	})
 });
 
