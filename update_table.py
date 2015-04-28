ddf6bcf499544974060fdf921160b4b5b57a8862
Shi <lfdorelli@gmail.com>
2015-04-27 23:21:02 -0300

    Blablabla

--- update_table.py
+++ update_table.py
@@ -1,30 +1,100 @@
 import urllib2
 import time
 import hashlib
+import json
 
-timestamp = int(time.time())
+problemas_player = {}
+players = {}
+teams = {}
+contest_res = []
+team_contests = []
+#  Dict -> player -> pos
+contests = { '203053' }
 
-params = { 
-	'time' :  str(timestamp),
-	'apiKey' : '32f81e4b7319652f10b46a8b3588ef022c717043',
-	'contestId' : '203053'
-}
+f = open ('scripts/config/score_individual', 'r')
+points = []
+for line in f:
+	val = int(line)
+	points.append(val)
 
-apiSecret = '620e9e83dd2827ec825f401f446b219f5792e2c9'
+for contest in contests:
+	contest_res.append({})
+	timestamp = int(time.time())
 
-request = 'contest.standings?'
-full_req = ''
+	params = { 
+		'time' :  str(timestamp),
+		'apiKey' : '32f81e4b7319652f10b46a8b3588ef022c717043',
+		'contestId' : contest
+	}
 
-for key in params:
-	request += key + '=' + params[key] + '&' 
+	apiSecret = '620e9e83dd2827ec825f401f446b219f5792e2c9'
 
-request = request[:-1]
-full_req = request
-request += '#' + apiSecret
+	request = 'contest.standings?'
+	full_req = ''
 
-res = hashlib.sha512('123456/' + request).hexdigest()
-full_req += '&apiSig=123456' + res 
-print "http://codeforces.com/api/"  + full_req
-res = urllib2.urlopen("http://codeforces.com/api/"  + full_req).read()
-print full_req, '\n'
-print res
+	for key in params:
+		request += key + '=' + params[key] + '&' 
+
+	request = request[:-1]
+	full_req = request
+	request += '#' + apiSecret
+
+	res = hashlib.sha512('123456/' + request).hexdigest()
+	full_req += '&apiSig=123456' + res 
+	# print "http://codeforces.com/api/"  + full_req
+	res = urllib2.urlopen("http://codeforces.com/api/"  + full_req).read()
+
+
+	jres = json.loads(res)
+	stand = jres['result']['rows']
+
+	pos = 0
+	for row in stand:
+		# if 'teamName' in row['party']:
+			# print 'Team ', row['party']['teamName']
+		if 'party' in row:
+			for player in row['party']['members']:
+				handle = player['handle']
+				players[handle] = 1
+				if handle not in problemas_player:
+					problemas_player[handle] = row['points']
+				else:
+					problemas_player[handle] += row['points']
+				contest_res[-1][player['handle']] = pos
+		pos = pos + 1
+
+# print contest_res
+
+penalty = {}
+points_player = {}
+contests_player = {}
+table_rows = []
+
+for player in players:
+	points_player[player] = 0
+	contests_player[player] = 0
+	penalty[player] = 0
+
+for contest in contest_res:
+	for handle in players:
+		if handle not in contest: 
+			if penalty[handle] == 0:
+				contest[handle] = 0
+			elif penalty[handle] == 1:
+				contest[handle] = -30
+			elif penalty[handle] == 2:
+				contest[handle] = -70
+			else:
+				contest[handle] = -100
+			penalty[handle] += 1
+
+	for handle in contest:
+		points_player[handle] += contest[handle]
+
+for player in points_player:
+	table_rows.append( [points_player[player], player] )
+
+print 'Handle #Contests #Problemas #Pontos'
+for row in table_rows:
+	handle = row[1]
+	print  handle, contests_player[handle], int(problemas_player[handle]), row[0] 
