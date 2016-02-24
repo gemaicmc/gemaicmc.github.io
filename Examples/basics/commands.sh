44690223cb21762092d019a5ae0832da30a706d3
6419011 <USUARIOS6419011@ltsp144.(none)>
2016-02-24 11:00:38 -0300

    Wow

---
+++ Examples/basics/commands.sh
@@ -0,0 +1,18 @@
+# Lista os arquivos do diretorio
+ls
+# Mostra o diretório em que você se encontra
+pwd
+# A saída do pwd mostra a hierarquia de pastas em que voce se encontra. Para voltar uma pasta,
+cd ..
+# Para ir para algum diretório, basta usar o comando cd também
+cd Documents
+# Para ir até a sua home (No CISC, a pasta com seu número USP)
+cd ~
+# Criando um diretório
+mkdir GEMA
+cd GEMA
+# Compilando um programa chamado exemplo.cpp
+g++ exemplo.cpp -o exemplo 
+# Vai ser gerado um arquivo executável chamado exemplo (sem a extensão cpp)
+# Para executá-lo
+./exemplo 
